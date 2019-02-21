68952a36fb296392ac7d187010af9e73ad91bbc5
Susumu Seino <seino@susu.mu>
2019-02-21 09:29:28 +0900

    tweak of conditional branch

--- wib-setup.sh
+++ wib-setup.sh
@@ -3,14 +3,8 @@
 
 if [ -e /bin/usr/gdrive ]; then
 	echo "Installed gdrive"
-fi
-if [ ! -e /bin/usr/gdrive ]; then
+else
 	echo "Installing gdrive..."
 	# gdriveをインストールする
 	wget "https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download" -O /usr/bin/gdrive
-
-	# 
-
 fi
-
-#wget "https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download" -O /usr/bin/gdrive

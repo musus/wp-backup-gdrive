da1a08722e7f4d7a2bcb977eafc6c4569d317f59
Susumu Seino <seino@susu.mu>
2019-02-21 10:24:24 +0900

    Make directory function

--- wib-setup.sh
+++ wib-setup.sh
@@ -2,8 +2,39 @@
 
 
 if [ -e /bin/usr/gdrive ]; then
-	echo "Installed gdrive"
+
+	echo "Installed gdrive - すでにgdriveがインストールされています."
+
 else
+
+function MakeDir() {
+    echo "バックアップ用のフォルダをGoogle Driveに作成しますか？"
+    echo "  yes または no と入力して下さい."
+    read inputMakeDir
+
+    if [ -z $inputMakeDir ]; then
+
+        echo "  yes または no を入力して下さい."
+        MakeDir
+
+    elif [ $inputMakeDir = 'yes' ] || [ $inputMakeDir = 'YES' ] || [ $inputMakeDir = 'y' ] ; then
+
+        gdrive mkdir "wp_backups"
+
+    elif [ $inputMakeDir = 'no' ] || [ $inputMakeDir = 'NO' ] || [ $inputMakeDir = 'n' ] ; then
+
+        echo "フォルダの作成を中止しました"
+
+    else
+
+        echo "  yes または no を入力して下さい."
+        MakeDir
+
+    fi
+
+
+}
+
 function ConfirmExecution() {
 
   echo "gdriveをインストールしますか?"
@@ -24,6 +55,7 @@ function ConfirmExecution() {
   elif [ $input = 'no' ] || [ $input = 'NO' ] || [ $input = 'n' ] ; then
 
     echo "  インストールを中断します."
+    MakeDir
     exit 1
 
   else
@@ -37,4 +69,19 @@ function ConfirmExecution() {
     ConfirmExecution
 
     echo "done"
+
+    # change ownership
+    sudo chown user /usr/bin/gdrive
+
+    # change mod
+    sudo chmod 755 /usr/bin/gdrive
+
+    # authenitication for Google
+    echo "下記URLにアクセスしGoogleアカウントを認証後、コードを入力してください."
+    gdrive list
+
+
+    MakeDir
+
 fi
+

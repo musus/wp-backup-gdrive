b49169c4b7a70328ddfbfbacd3e46d2e9ea59930
Susumu Seino <seino@susu.mu>
2019-02-21 09:54:15 +0900

    Add interactive format

--- wib-setup.sh
+++ wib-setup.sh
@@ -4,7 +4,37 @@
 if [ -e /bin/usr/gdrive ]; then
 	echo "Installed gdrive"
 else
-	echo "Installing gdrive..."
+function ConfirmExecution() {
+
+  echo "gdriveをインストールしますか?"
+  echo "  yes または no と入力して下さい."
+  read input
+
+  if [ -z $input ] ; then
+
+    echo "  yes または no を入力して下さい."
+    ConfirmExecution
+
+  elif [ $input = 'yes' ] || [ $input = 'YES' ] || [ $input = 'y' ] ; then
+
+    echo "  インストール中."
 	# gdriveをインストールする
 	wget "https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download" -O /usr/bin/gdrive
+
+  elif [ $input = 'no' ] || [ $input = 'NO' ] || [ $input = 'n' ] ; then
+
+    echo "  インストールを中断します."
+    exit 1
+
+  else
+
+    echo "  yes または no を入力して下さい."
+    ConfirmExecution
+
+  fi
+
+}
+    ConfirmExecution
+
+    echo "done"
 fi
